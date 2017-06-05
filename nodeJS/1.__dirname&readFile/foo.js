eedd861b819c3dfc87b6a06ea6d9256fd2af18c0
RuiWang <ruiwang@qf-SteveGuo-Mac.local>
2017-06-05 18:47:30 +0800

    数组去重

--- nodeJS/1.__dirname&readFile/foo.js
+++ nodeJS/1.__dirname&readFile/foo.js
@@ -2,9 +2,9 @@
  * Created by ruiwang on 2017/6/2.
  */
 var fs=require('fs');
-fs.readFile("./test.js","utf8",function(err,data){
+fs.readFile(__dirname+"./test.js","utf8",function(err,data){
     console.log(data);
     if(err){
         console.log(err);
     }
-})
+})   //有点问题,先不急着使用
